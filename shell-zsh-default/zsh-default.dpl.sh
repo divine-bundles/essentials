9710084f8afe5de91ad056f40e8c6326080e165f
Grove Pyree <grayarea@protonmail.ch>
2019-07-07 21:38:40 +0300

    shell-zsh-default: fix wording on sudo warning before chsh -s

--- shell-zsh-default/zsh-default.dpl.sh
+++ shell-zsh-default/zsh-default.dpl.sh
@@ -224,7 +224,7 @@ dinstall()
   local old_shell="$SHELL"
 
   # Now, CHange SHell with chsh (user password will be prompted for)
-  dprint_start -l 'Changing default shell requires sudo password'
+  dprint_start -l 'Changing default shell requires user password'
   if chsh -s "$D_ZSH_PATH"; then
     dprint_debug 'Successfully changed default shell to:' -i "$D_ZSH_PATH"
     dstash -s set old_shell "$old_shell"
@@ -296,7 +296,7 @@ dremove()
 
     # Execute change
     if $shell_is_ok; then
-      dprint_start -l 'Changing default shell requires sudo password'
+      dprint_start -l 'Changing default shell requires user password'
     fi
     if $shell_is_ok && chsh -s "$old_shell"; then
       dprint_debug 'Successfully restored default shell to:' -i "$old_shell"
