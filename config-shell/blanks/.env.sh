a386d6e45f35ee7aff44d012420275b9eee83a62
Grove Pyree <grayarea@protonmail.ch>
2019-07-22 15:57:26 +0300

    Trim comments from blank files

--- config-shell/blanks/.env.sh
+++ config-shell/blanks/.env.sh
@@ -1,12 +1,3 @@
-#!/usr/bin/env bash
-#:title:        Divine.dotfiles, 'config-shell', .env.sh
-#:author:       Grove Pyree
-#:email:        grayarea@protonmail.ch
-#:revnumber:    0.0.1-SNAPSHOT
-#:revdate:      2019.07.22
-#:revremark:    Initial revision
-#:created_at:   2019.07.22
-
 ## Part of Divine.dotfiles <https://github.com/no-simpler/divine-dotfiles>
 #
 ## Divine deployments 'core' package
